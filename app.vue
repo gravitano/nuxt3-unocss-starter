cd46586b6e70ea0952a1e2f3a9791fd3942c7143
Warsono <warsono16694@gmail.com>
2022-04-18 00:23:16 +0700

    chore: add unocss

--- app.vue
+++ app.vue
@@ -1,5 +1,19 @@
 <template>
-  <div>
-    <NuxtWelcome />
+  <div class="container mx-auto">
+    <h1 class="text-xl font-bold">Hello World</h1>
+    <div class="alert-info">
+      <span class="i-carbon-home"></span>
+      Your profile has beend updated!
+    </div>
+    <div class="text-gray-500">lorem ipsum</div>
+    <input type="text" name="name" value="" class="form-input" />
+    <button type="button" class="btn-primary">Button</button>
   </div>
 </template>
+
+<style>
+body {
+  font-family: sans-serif;
+  font-size: 14px;
+}
+</style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,33 @@
-import { defineNuxtConfig } from 'nuxt3'
+import { defineNuxtConfig } from 'nuxt3';
 
 // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
+  buildModules: ['@unocss/nuxt'],
+  unocss: {
+    // presets
+    preflight: true,
+    uno: true, // enabled `@unocss/preset-uno`
+    icons: true, // enabled `@unocss/preset-icons`
+    attributify: true, // enabled `@unocss/preset-attributify`,
+    typography: true,
 
-})
+    webFonts: true,
+
+    // core options
+    shortcuts: [
+      {
+        'form-input':
+          'px-2 py-2 border border-gray-500 rounded-md w-full focus:ring-2 focus:ring-blue-300 focus:ring-opacity-50 focus:border-blue-500 focus:outline-none',
+      },
+      {
+        'btn-primary':
+          'px-4 py-2 border borde-blue-500 rounded-lg appearance-none bg-blue-500 text-white inline-flex justify-center hover:bg-blue-600',
+      },
+      {
+        'alert-info':
+          'px-2 py-2 rounded border bg-sky-500 text-white border-sky-500 w-full flex',
+      },
+    ],
+    rules: [],
+  },
+});
